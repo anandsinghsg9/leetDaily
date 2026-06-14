09974417628296809097a8540388c1e90a5e2b4b
Anand Singh <153064775+anandsinghsg9@users.noreply.github.com>
2026-06-15 03:45:03 +0530

    Time: 18 ms (6.02%), Space: 46.9 MB (74.67%) - LeetHub - Time: O(n), Space: O(1)

--- 0707-design-linked-list/0707-design-linked-list.java
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -1,88 +1,104 @@
 class MyLinkedList {
-    int data;
-    MyLinkedList next;
-    MyLinkedList head;
-    public MyLinkedList(){
-        this.head=null;
-    }
-    public MyLinkedList(int data){
-        this.data=data;
-        this.next=null;
+    class Node{
+        int data ;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
     }
 
+    Node head;
+
+    public MyLinkedList() {
+        head=null;
+    }
+
+    public int count(Node head){
+        if(head==null){
+            return 0;
+        }
+        Node temp=head;
+        int count=1;
+        while(temp.next!=null){
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+    
     public int get(int index) {
-        if(index<0 || head==null){
+        if(index>=count(head)){
             return -1;
         }
-        int count=0;
-        MyLinkedList temp=head;
-        while(count<index && temp!=null){
+        Node temp=head;
+        while(index>0){
             temp=temp.next;
-            count++;
+            index--;
         }
-        return (temp==null)? -1:temp.data;
+        return temp.data;
     }
     
     public void addAtHead(int val) {
-        MyLinkedList temp=new MyLinkedList(val);
-        temp.next=head;
-        head=temp;
+        Node newNode=new Node(val);
+        if(count(head)==0){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
     }
     
     public void addAtTail(int val) {
-        MyLinkedList temp=head;
-        MyLinkedList add=new MyLinkedList(val);
-        if(head==null ){
-            head=add;
+        Node newNode=new Node(val);
+        int len=count(head);
+        if(len==0){
+            head=newNode;
             return;
         }
-        while(temp.next!=null){
+        Node temp=head;
+        while(len>1){
             temp=temp.next;
+            len--;
         }
-        temp.next=add;
+        temp.next=newNode;
     }
     
     public void addAtIndex(int index, int val) {
-        MyLinkedList add=new MyLinkedList(val);
-        if(index==0 ){
+        int len=count(head);
+        if(index>len){
+            return;
+        }
+        Node newNode=new Node(val);
+        Node temp=head;
+        if(index==0){
             addAtHead(val);
             return;
         }
-        if(head==null){
-            return;
-        }
-        int count=0;
-        MyLinkedList temp=head;
-        while(count<(index-1) && temp!=null){
+        while(index>1){
             temp=temp.next;
-            count++;
+            index--;
         }
-        if(temp==null){
-            return;
-        }
-        add.next=temp.next;
-        temp.next=add;
+        newNode.next=temp.next;
+        temp.next=newNode;
     }
     
     public void deleteAtIndex(int index) {
-        if(head==null || index<0){
+        int len=count(head);
+        if(len<=index){
             return;
         }
         if(index==0){
             head=head.next;
             return;
         }
-        int count=0;
-        MyLinkedList temp=head;
-        while(count<(index-1) && temp!=null){
+        Node temp=head;
+        while(index>1){
             temp=temp.next;
-            count++;
+            index--;
         }
-        if(temp==null || temp.next==null){
-            return;
-        }
-        
         temp.next=temp.next.next;
+
     }
 }
 
@@ -98,4 +114,4 @@ class MyLinkedList {
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
