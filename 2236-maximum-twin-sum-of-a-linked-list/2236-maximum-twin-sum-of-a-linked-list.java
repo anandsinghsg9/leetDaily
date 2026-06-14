cb6abcbae70c8ad9bb8298002ee395f89b852b3b
Anand Singh <153064775+anandsinghsg9@users.noreply.github.com>
2026-06-14 16:33:34 +0530

    Time: 5 ms (58.5%), Space: 97 MB (99.61%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        if(head==null){
+            return 0;
+        }
+        ListNode slowPoint=new ListNode();
+        ListNode fastPoint=new ListNode();
+        slowPoint=head;
+        fastPoint=head;
+        while(fastPoint!=null){
+            slowPoint=slowPoint.next;
+            fastPoint=fastPoint.next.next;
+        }
+        ListNode prev=new ListNode();
+        prev=null;
+        while(slowPoint!=null){
+            ListNode next=new ListNode();
+            next=slowPoint.next;
+            slowPoint.next=prev;
+            prev=slowPoint;
+            slowPoint=next;
+        }
+        slowPoint=prev;
+        int output=0;
+        fastPoint=head;
+        while(slowPoint!=null){
+            int step=fastPoint.val+slowPoint.val;
+            output=Math.max(step,output);
+            fastPoint=fastPoint.next;
+            slowPoint=slowPoint.next;
+        }
+        return output;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
