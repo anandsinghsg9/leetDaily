97d4250a7a9aef442299b7ce3915ef0725f6e477
Anand Singh <153064775+anandsinghsg9@users.noreply.github.com>
2026-06-16 10:04:56 +0530

    Time: 4 ms (76.47%), Space: 55.6 MB (28.68%) - LeetHub - Time: O(n^2), Space: O(n)

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -2,17 +2,18 @@ class Solution {
     public String processStr(String s) {
         StringBuilder a=new StringBuilder();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='*'){
+            char ch=s.charAt(i);
+            if(ch=='*'){
                 a=(a.length()==0)? a:a.deleteCharAt(a.length()-1);
             }
-            else if(s.charAt(i)=='#'){
+            else if(ch=='#'){
                 a.append(a.toString());
             }
-            else if(s.charAt(i)=='%'){
+            else if(ch=='%'){
                 a.reverse();
             }
             else{
-                a.append(s.charAt(i));
+                a.append(ch);
             }
 
         }
