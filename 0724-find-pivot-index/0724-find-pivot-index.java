fb5913b59f2835e640c64fefc79850503e132a8f
Anand Singh <153064775+anandsinghsg9@users.noreply.github.com>
2026-06-20 02:56:05 +0530

    Time: 1 ms (98.53%), Space: 47.5 MB (21.98%) - LeetHub - Time: O(n), Space: O(n)

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,17 +1,25 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int rightsum=0;
-        for(int i=0;i<nums.length;i++){
-            rightsum+=nums[i];
+        int len=nums.length;
+        int[] leftSum=new int[len+1];
+        int [] rightSum=new int[len+1];
+        leftSum[0]=0;
+        rightSum[len]=0;
+        for(int i=1;i<len+1;i++){
+            leftSum[i]=leftSum[i-1]+nums[i-1];
         }
-        int leftsum=0;
-        for(int i=0;i<nums.length;i++){
-            rightsum-=nums[i];
-            if(leftsum==rightsum){
+        for(int i=len-1;i>(-1);i--){
+            rightSum[i]=rightSum[i+1]+nums[i];
+        }
+        for(int i=0;i<len;i++){
+            if(leftSum[i]==rightSum[i+1]){
                 return i;
             }
-            leftsum+=nums[i];    
         }
         return -1;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
