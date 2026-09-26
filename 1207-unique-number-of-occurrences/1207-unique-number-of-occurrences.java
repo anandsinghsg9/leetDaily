a1c42b7cd8c06115b5f35a5e2fd3a6996b5de097
Anand Singh <153064775+anandsinghsg9@users.noreply.github.com>
2026-09-26 21:10:29 +0530

    Time: 2 ms (97.94%), Space: 43.8 MB (18.68%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(set.contains(entry.getValue())){
+                return false;
+            }
+            set.add(entry.getValue());
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
